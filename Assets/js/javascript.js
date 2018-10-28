a2c5c34e8a09dbce3caf8f500612fb3dae60f363
James Bell <Noxachron@gmail.com>
2018-10-28 19:53:22 -0400

    cleaned up a little

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,18 +1,19 @@
 'use strict';
+let STORE = [];
+
 // check out todos by clickie
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed")
 });
 // click on x to delete
-$("ul").on("click", "span", function(event){
-	for(let i = 0; i<STORE.length; i++){
-		if($(this).parent()[0].textContent === STORE[i].name){
-			STORE[i].deleteMe = true;
+$("ul").on("click", "span", function(event){					//span is the (trash icon) html child element on the li
+	for(let i = 0; i<STORE.length; i++){						//loops thru STORE
+		if($(this).parent()[0].textContent === STORE[i].name){	//finds any matches for the text of the line on which the user has clicked trash icon.
+			STORE[i].deleteMe = true;							//and flags them to be removed
 		}
 	}
 	removeFromStore();
 	event.stopPropagation();
-	renderList();
 });
 
 // add listener to input[text] class add
@@ -28,15 +29,14 @@ $(".add").keypress(function(event){
 
 $(".del").keypress(function(event){
 	if(event.which === 13 ){				//code 13 for enter
-		let deleteText = $(this).val();
+		let deleteText = $(this).val();		    //sets a variable to what the user has typed into Remove Todo Input-box
 		for(let i = 0; i<STORE.length; i++){
-			if(STORE[i].name === deleteText){
-				STORE[i].deleteMe = true;
+			if(STORE[i].name === deleteText){	//finds all the store items that match
+				STORE[i].deleteMe = true;		//flags matches to be removed
 			}
 		}
 		$(this).val(""); // blanks input for next user input
 		removeFromStore();
-		renderList();
 	}
 });
 
@@ -45,8 +45,6 @@ $("#fontTrash").click(function(){
 	$("input[type='text']").fadeToggle();
 });
 
-let STORE = [];
-
 function renderList(){
 	let showStore = STORE.map(index => index.showHtml);
 	$("ul").html(showStore);
@@ -54,10 +52,11 @@ function renderList(){
 
 function removeFromStore(){
 	STORE = STORE.filter(element => (element.deleteMe === false));
+	renderList();
 }
 
 function init(){
 	renderList();
-};
+}
 
 init();
