cbda43c94f0b3dfb142332ba7b4e49df3bf56d73
James Bell <Noxachron@gmail.com>
2018-10-28 19:02:45 -0400

    moved DOM list to Store

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,13 +1,18 @@
+'use strict';
 // check out todos by clickie
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed")
 });
 // click on x to delete
 $("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500, function(){
-		$(this).remove()
-	})
+	for(let i = 0; i<STORE.length; i++){
+		if($(this).parent()[0].textContent === STORE[i].name){
+			STORE[i].deleteMe = true;
+		}
+	}
+	removeFromStore();
 	event.stopPropagation();
+	renderList();
 });
 
 // add listener to input[text]
@@ -15,13 +20,30 @@ $("input[type='text']").keypress(function(event){
 	if(event.which === 13 ){				//code 13 for enter
 		var todoText =	$(this).val();
 		//append a new li to the ul and then apply todoText to it
-		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
-		 $(this).val(""); // blanks input for next user input
+		STORE.push({showHtml: `<li><span><i class='fas fa-trash'></i></span>${todoText}</li>`, name: `${todoText}`, deleteMe: false});
+		$(this).val(""); // blanks input for next user input
+		renderList();
 	}
 });
+
 //show or hide the add new list input bar
 $("#fontTrash").click(function(){
 	$("input[type='text']").fadeToggle();
 });
 
+let STORE = [];
 
+function renderList(){
+	let showStore = STORE.map(index => index.showHtml);
+	$("ul").html(showStore);
+}
+
+function removeFromStore(){
+	STORE = STORE.filter(element => (element.deleteMe === false));
+}
+
+function init(){
+	renderList();
+};
+
+init();
